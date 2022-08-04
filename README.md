80f15bce150903bba4d37e9ca2f67e71f4241112
je1g19 <je1g19@soton.ac.uk>
2022-08-04 17:49:28 +0100

    House keeping

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # raytracer
+
+build source with:
+g++.exe -fdiagnostics-color=always -g C:\Users\Joel\Desktop\C++\raytracer\raytracer.cpp -o C:\Users\Joel\Desktop\C++\raytracer\raytracer.exe
+
+run tracer with:
+.\raytracer.exe > image.ppm
--- colour.h
+++ colour.h
@@ -7,7 +7,11 @@
 
 void write_colour(std::ostream &out, colour pixel_colour)
 {
-    out << static_cast<int>(255.999) * pixel_colour.x()) << ' '
-        << static_cast<int>(255.999) * pixel_colour.y()) << ' '
-        << static_cast<int>(255.999) * pixel_colour.z()) << '\n'
+    out << static_cast<int>((255.999) * pixel_colour.x()) << ' '
+        << static_cast<int>((255.999) * pixel_colour.y()) << ' '
+        << static_cast<int>((255.999) * pixel_colour.z()) << '\n';
 }
+
+
+
+#endif 
