a33978bb204640d5f8cc08741745aed58f5c9738
je1g19 <je1g19@soton.ac.uk>
2022-08-05 18:56:52 +0100

    Chapter 6.2 Sphere intersection code optimisation

--- raytracer.cpp
+++ raytracer.cpp
@@ -4,28 +4,39 @@
 #include "ray.h"
 
 //return true if the ray intersects a defined sphere
-bool hit_sphere(const point3& centre, double radius, const ray& r)
+double hit_sphere(const point3& centre, double radius, const ray& r)
 {
     vec3 co = r.origin() - centre;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0*dot((r.direction()), co);
-    auto c = dot(co,co) - radius*radius;
+    auto a = r.direction().length_squared();
+    auto half_b = dot((r.direction()), co);
+    auto c = co.length_squared() - radius*radius;
 
-    auto discriminant = b*b - 4*a*c;
+
+    auto discriminant = half_b*half_b - a*c;
     
-    return (discriminant > 0);
+    if(discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-half_b - sqrt(discriminant)) / (a);
+    }
 
 }
 
 colour ray_colour(const ray& r)
 {
-    if(hit_sphere(point3(0,0,-1) , 0.5, r))
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r); //returns the t value for where the ray intersects the sphere
+    if (t > 0.0)
     {
-        
-        return colour(1,0,0);
+        vec3 N = unit_vector(r.at(t)-vec3(0,0,-1)); // N is the vector from the centre of the sphere to the point of intersection, normalised.
+        return 0.5*colour(N.x()+1, N.y()+1, N.z()+1); // Map N to a rgb value 0-1
     }
+
+    //Blue gradient background
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*colour(1.0,1.0,1.0) + t*colour(0.5,0.7,1.0);
 }
 
