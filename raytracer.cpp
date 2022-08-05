915ce98c98096896bdf0f89be73f3b0fbd593cd1
je1g19 <je1g19@soton.ac.uk>
2022-08-05 16:30:19 +0100

    Finished chapter 5.2 added hit_sphere function

--- raytracer.cpp
+++ raytracer.cpp
@@ -3,13 +3,34 @@
 #include "colour.h"
 #include "ray.h"
 
+//return true if the ray intersects a defined sphere
+bool hit_sphere(const point3& centre, double radius, const ray& r)
+{
+    vec3 co = r.origin() - centre;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0*dot((r.direction()), co);
+    auto c = dot(co,co) - radius*radius;
+
+    auto discriminant = b*b - 4*a*c;
+    
+    return (discriminant > 0);
+
+}
 
 colour ray_colour(const ray& r)
 {
+    if(hit_sphere(point3(0,0,-1) , 0.5, r))
+    {
+        
+        return colour(1,0,0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*colour(1.0,1.0,1.0) + t*colour(0.5,0.7,1.0);
 }
+
+
+
 
 int main()
 {
